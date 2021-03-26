539bb09a148da24507bb6ccfa068eac4c5ff8970
Hajar benzaouia <hajar.benzaouia@gmail.com>
2021-03-26 03:47:49 +0100

    jupyter without requestion password and token

--- base-notebook/jupyter_notebook_config.py
+++ base-notebook/jupyter_notebook_config.py
@@ -10,6 +10,10 @@ import stat
 c = get_config()  # noqa: F821
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
+#to disable the password
+c.NotebookApp.password = u''
+c.NotebookApp.token = ''
+#############################
 c.NotebookApp.open_browser = False
 
 # https://github.com/jupyter/notebook/issues/3130
